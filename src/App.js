6db9be6125b9326b361a30faaee95cef418b0a52
ning1315 <59202849+ning1315@users.noreply.github.com>
2021-01-30 00:09:40 +0900

    set actions

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-     <img src="http://image.tmdb.org/t/p/w200/glKDfE6btIRcVB5zrjspRIs4r52.jpg"></img>
+     <img src="http://image.tmdb.org/t/p/w200/1ihJ0yr7v6YqP6zvPVpPKUrOuQ3.jpg"></img>
     </div>
   );
 }
--- store/actions/index.js
+++ store/actions/index.js
@@ -9,6 +9,10 @@ export const FETCH_HORROR_MOVIES = 'FETCH_HORROR_MOVIES';
 export const FETCH_ROMANCE_MOVIES = 'FETCH_ROMANCE_MOVIES';
 export const FETCH_DOCUMENTARIES = 'FETCH_DOCUMENTARIES';
 
+
+const BASE_URL = 'https://api.themoviedb.org/3/'
+const API_KEY = 'f6c50ad2d5427e9942a38f4309510e10'
+
 export const fetchTrendData = (data) => {
   return {
     type : FETCH_TRENDING,
@@ -16,8 +20,140 @@ export const fetchTrendData = (data) => {
   }
 }
 
+export const fetchNetplixOriginalData = (data) => {
+  return {
+    type : FETCH_NETPLIX_ORIGINALS,
+    data
+  }
+}
+
+export const fetchTopRateData = (data) => {
+  return {
+    type : FETCH_TOP_RATED,
+    data
+  }
+}
+
+export const fetchActionMoviesData = (data) => {
+  return {
+    type : FETCH_ACTION_MOVIES,
+    data
+  }
+}
+
+export const fetchComedyMoviesData = (data) => {
+  return {
+    type : FETCH_COMEDY_MOVIES,
+    data
+  }
+}
+
+export const fetchHorrorMoviesData = (data) => {
+  return {
+    type : FETCH_HORROR_MOVIES,
+    data
+  }
+}
+
+export const fetchRomanceMoviesData = (data) => {
+  return {
+    type : FETCH_ROMANCE_MOVIES,
+    data
+  }
+}
+
+export const fetchDocumentariesMoviesData = (data) => {
+  return {
+    type : FETCH_DOCUMENTARIES,
+    data
+  }
+}
+
 export const fetchTrending = () => {
   return (dispatch) => {
-    return axios.get(``)
+    return axios.get(`${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`)
+    .then((res) => {
+      dispatch(fetchTrendData(res.data))
+    })
+    .catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const fetchNetplixOriginal = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/discover/tv?api_key=${API_KEY}&with_networks=213`)
+    .then((res) => {
+      dispatch(fetchNetplixOriginalData(res.data))
+    }).catch((err) =>{
+      throw(err)
+    })
+  }
+}
+
+export const fetchTopRate = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`)
+    .then((res) => {
+      dispatch(fetchTopRateData(res.data))
+    }).catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const fetchActionMovies = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=28`)
+    .then((res) => {
+      dispatch(fetchActionMoviesData(res.data))
+    }).catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const fetchComedymovies = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=35`)
+    .then((res) => {
+      dispatch(fetchComedyMoviesData(res.data))
+    }).catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const fetchHorrorMovies = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=27`)
+    .then((res) => {
+      dispatch(fetchHorrorMoviesData(res.data))
+    }).catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const fetchRomanceMovies = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=10749`)
+    .then((res) => {
+      dispatch(fetchRomanceMoviesData(res.data))
+    }).catch((err) => {
+      throw(err)
+    })
+  }
+}
+
+export const fetchDocumentarieMovies = () => {
+  return (dispatch) => {
+    return axios.get(`${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=99`)
+    .then((res) => {
+      dispatch(fetchDocumentariesMoviesData(res.data))
+    }).catch((err) => {
+      throw(err)
+    })
   }
 }
